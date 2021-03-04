c67c980bdb8ad03b0150d52341218908714ea016
ark9719 <ark9719@g.rit.edu>
2021-03-04 02:59:46 -0500

    Working script

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 Alert me for NYC Javits center appointment availability
+
+Checks public NYC API for availability every 5 Minutes
+Uses GMAIL SMTP to alert for appointments
+
+-requires 'requests'
+
+-expects config.py file in root
+(gmail accounts with 2-factor need to create an app password)
+```
+gmail=[your gmail here]
+password=[your gmail password here]
+```
+
+-written on python 3.8
---
+++ find_vaccine.py
@@ -0,0 +1,35 @@
+import requests
+import smtplib
+from email.message import EmailMessage
+import threading
+import sys
+import config
+
+
+def check():
+    threading.Timer(300.0, check).start()
+    r = requests.get('https://am-i-eligible.covid19vaccine.health.ny.gov/api/list-providers')
+    providers = r.json()
+
+    for provider in providers['providerList']:
+        #Javits Center
+        if provider['providerId'] == 1000: 
+            available_apts = provider['availableAppointments']
+
+            if available_apts != 'NAC':
+                print("Appointments availabile!")
+                msg = EmailMessage()
+                msg['Subject'] = 'Appointments available at Javits Center'
+                msg['From'] = 'nyc_vaccine_check'
+                msg['To'] = config.gmail
+                s = smtplib.SMTP('smtp.gmail.com', 587)
+                s.starttls()
+                s.login(config.gmail, config.password)
+                s.send_message(msg)
+                s.quit()
+                sys.exit()
+            else:
+                print("No appointments")
+
+
+check()
